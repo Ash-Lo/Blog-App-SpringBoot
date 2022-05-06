1873f7ba8e73daed42204907827207a354bb748b
Ash-Lo <ashutosh201998@gmail.com>
2022-05-06 13:07:37 +0530

    Post resource crud push

---
+++ spring-boot-blog-rest-apis/src/test/java/com/springboot/blog/springbootblogrestapis/SpringBootBlogRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blog.springbootblogrestapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBlogRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
