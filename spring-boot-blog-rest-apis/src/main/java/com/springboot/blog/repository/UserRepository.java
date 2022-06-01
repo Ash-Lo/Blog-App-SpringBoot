50118be377388619721323451d2ceb93171ad7c3
Ash-Lo <ashutosh201998@gmail.com>
2022-06-01 14:13:03 +0530

    Securing Rest API's - bug encountered

---
+++ spring-boot-blog-rest-apis/src/main/java/com/springboot/blog/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.springboot.blog.repository;
+
+import java.util.Optional;
+
+import com.springboot.blog.entity.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+}
